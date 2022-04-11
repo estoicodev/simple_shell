664a5cb42fe1969f191c9d874af768d716b3354d
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-04-11 16:44:38 -0700

    Add get_input, compare_builtins, handle_child_process functions

--- principal_functions.c
+++ principal_functions.c
@@ -38,3 +38,101 @@ void prompt(void)
 		write(STDOUT_FILENO, pr, _strlen(pr));
 }
 
+
+/**
+ * get_input - desc
+ * Return: Input string
+ */
+char *get_input(void)
+{
+	char *line = NULL;
+	ssize_t n;
+	size_t size = 0;
+
+	n = getline(&line, &size, stdin);
+	if (line == NULL)
+	{
+		perror("Error allocating memory for buffer");
+		return (0);
+	}
+	else if (n == 1)
+	{
+		free(line);
+		return (NULL);
+	}
+	else if (n == EOF)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "\n", 1);
+		free(line);
+		exit(0);
+	}
+	return (line);
+}
+
+/**
+ * compare_builtins - desc
+ * @ar: ...
+ * @line: ...
+ * Return: 1 (Find a success compare)
+ */
+int compare_builtins(char **ar, char *line)
+{
+	if (write_exit(ar) == 1)
+	{
+		free_ar(ar);
+		free(line);
+		exit(0);
+	}
+	if (write_env(ar) == 1)
+	{
+		fprintenv(environ);
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * handle_child_process - desc
+ * @ar: ...
+ * @av: ...
+ * @cnt: ...
+ * @line: ...
+ * Return: 1 (Success). It fails -1
+ */
+int handle_child_process(char **ar, char **av, int cnt, char *line)
+{
+	pid_t pid;
+
+	if (ar == NULL)
+		return (-1);
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror(av[0]);
+		free_ar(ar);
+		free(line);
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(ar[0], ar, environ) == -1)
+		{
+			print_error(av[0], cnt, ar[0]);
+			free_ar(ar);
+			free(line);
+			exit(1);
+		}
+		free_ar(ar);
+		free(line);
+		exit(0);
+	}
+	else
+	{
+		wait(NULL);
+	}
+
+	return (1);
+}
