94136580e2ec54e45e299359c242a1fe99d7e3c4
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-04-11 14:58:04 -0700

    Add header file, main, and functions

---
+++ main.c
@@ -0,0 +1,38 @@
+#include "header.h"
+
+/**
+ * main - main function
+ * @ac: ...
+ * @av: arguments of the programm
+ * Return: 0 is success, Otherwise 1
+ */
+int main(int ac __attribute__ ((unused)), char *av[])
+{
+	char *line = NULL, **ar;
+	int count = 0;
+
+	while (1)
+	{
+		prompt();
+
+		line = get_input();
+		if (!line)
+		{
+			count++;
+			continue;
+		}
+
+		ar = split_string(line, " \t\n");
+
+		if (compare_builtins(ar, line) == 1)
+			count++;
+
+		if (_strcmp(ar[0], "env") != 0)
+		{
+			count++;
+			validation(ar, get_PATHS());
+			handle_child_process(ar, av, count, line);
+		}
+	}
+	return (0);
+}
