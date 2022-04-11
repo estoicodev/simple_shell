6a81ef0562e8ab5f02ba2c47efb3c8e2bcfbc6cb
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-04-11 18:49:45 +0000

    Add split_string that return an array of tokens with a delimitator

---
+++ split_string.c
@@ -0,0 +1,80 @@
+#include "header.h"
+
+/**
+ * count_tokens - The size of words in the string separate by space
+ * @str: String
+ *
+ * Return: The size of words
+ * Otherwise, -1
+ */
+int count_tokens(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; *str; i++)
+	{
+		if (i == 0 && *str == ' ')
+		{
+			while (*(str + 1) == ' ')
+				str++;
+		}
+		else if (*str == ' ')
+		{
+			while (*(str + 1) == ' ')
+				str++;
+			count++;
+		}
+		str++;
+	}
+	count++;
+
+	return (count);
+}
+
+/**
+ * split_string - split string by space and returns an array of each word
+ * @str: String
+ * @delim: Delimitator
+ *
+ * Return: Array of each word of the string,
+ * Otherwise, NULL
+ */
+char **split_string(char *str, char *delim)
+{
+	int ar_len, i = 0;
+	char *copy, *token;
+	char **toks;
+
+	ar_len = count_tokens(str);
+
+	toks = malloc((ar_len + 1) * sizeof(char *));
+	if (toks == NULL)
+		return (NULL);
+
+	copy = _strdup(str);
+	if (copy == NULL)
+	{
+		free(toks);
+		return (NULL);
+	}
+
+	token = strtok(copy, delim);
+	while (token != NULL)
+	{
+		toks[i] = _strdup(token);
+		if (toks[i] == NULL)
+		{
+			ar_len = i;
+			for (i = 0; i <= ar_len; i++)
+				free(toks[i]);
+			free(toks);
+			return (NULL);
+		}
+		token = strtok(NULL, delim);
+		i++;
+	}
+	toks[i] = token;
+	free(copy);
+
+	return (toks);
+}
