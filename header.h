2bedf6f5801289c8b626fa12363a76eaa71b94dc
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-04-11 23:09:10 +0000

    .

--- header.h
+++ header.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <unistd.h>
 
 extern char **environ;
--- helper_functions.c
+++ helper_functions.c
@@ -21,13 +21,96 @@ void free_ar(char **ar)
 }
 
 /**
- * debug - function that debuggs line and ar
- * @line: Basic command line entered by the user
- * @ar: Basic command line entered by the user
- * Return: 1 (Success). Otherwise 0
+ * tokalloc - allocate memory space to the new array of tokens
+ * @buffer: a string to be used to create the array of tokens
+ * @delim: the delimiters to be used to separate the new tokens
+ * @ctok: the number of tokens to be created
+ *
+ * Return: 0 if the function works succesfully, or -1 on error
  */
-void debug(char *line, char **ar)
+char **tokalloc(char *buffer, char *delim, int ctok)
 {
-	printf("line = %s\n", line);
-	printf("ar[0] = %s\n", ar[0]);
+	int i, k = 0;
+	char **tokens;
+
+	tokens = malloc((ctok + 1) * sizeof(char *));
+	if (tokens == NULL)
+	{
+		free(tokens);
+		return (NULL);
+	}
+	for (i = 0; i <= ctok; i++)
+	{
+		if (*buffer == '\0')
+		{
+			tokens[i] = NULL;
+			break;
+		}
+		while (*buffer != delim[0] && *buffer != delim[1])
+		{
+			k++;
+			buffer++;
+		}
+		tokens[i] = malloc((k + 1) * sizeof(char));
+
+		if (tokens[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(tokens[i]);
+			free(tokens);
+			return (NULL);
+		}
+		buffer++;
+		k = 0;
+	}
+	return (tokens);
+}
+
+/**
+ * _strtok_all - Tokenize buffers with given parameters
+ * @buffer: buffer to process
+ * @delimiter: string that will separate tokens
+ *
+ * Return: Array of tokens
+ */
+char **_strtok_all(char *buffer, char *delimiter)
+{
+	int i, j, ctok = 0, k = 0;
+	char *ptr = buffer, *ptr2 = buffer;
+	int size = _strlen(buffer);
+	char **tokens = NULL;
+
+	if (buffer == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			if (buffer[i] == delimiter[j])
+				ctok++;
+		}
+	}
+	tokens = tokalloc(ptr, delimiter, ctok);
+
+	if (tokens == NULL)
+		return (NULL);
+	ptr = NULL;
+
+	for (i = 0; i <= ctok; i++)
+	{
+		if (*ptr2 == '\0')
+			break;
+		while (*ptr2 != delimiter[0] && *ptr2 != delimiter[1])
+		{
+			tokens[i][k] = *ptr2;
+			ptr2++;
+			k++;
+		}
+		tokens[i][k] = '\0';
+		ptr2++;
+		k = 0;
+	}
+	ptr2 = NULL;
+	return (tokens);
 }
